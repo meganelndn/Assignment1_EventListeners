e8fc10d38caf7136163f33548b7fe335b1211efa
megane <megane.londono@hotmail.com>
2021-09-23 22:00:48 +0200

    all events done except for use of objects

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /* click */
-const clickBtn = document.querySelector(".click");
+let btn = document.querySelector("#btn");
 
-clickBtn.addEventListener("click", e => {
-    e.preventDefault();
-    clickBtn.classList.add("btnStyle");
-    clickBtn.innerText = "Cool, you did it!";
+btn.addEventListener("click", e => {
+    let ddlPets = document.getElementById("ddlPets");
+    let selectedText = ddlPets.options[ddlPets.selectedIndex].innerHTML;
+    alert(`You chose: ${selectedText}`);
 })
 
 /* mouseover */
 const mouseoverBtn = document.querySelector(".mouseover");
 
 mouseoverBtn.addEventListener("mouseover", e => {
-    mouseoverBtn.innerText = "Amazing, thanks";
+    mouseoverBtn.textContent = "Amazing, thanks";
 })
 
 /* focus */
@@ -19,14 +19,24 @@ const focusInput = document.querySelector(".focus");
 
 focusInput.addEventListener("focus", e => {
     focusInput.classList.add("focusBg");
-    document.querySelector(".focusLabel").innerText = "Wow, you're killing it."
+    document.querySelector(".focusLabel").textContent = "Wow, you're killing it."
 })  
 
 /* keydown */
 const keydownInput = document.querySelector(".keydown");
 
+let keyArray = [];
+
 keydownInput.addEventListener("keydown", e => {
-    document.querySelector(".keydownLabel").innerText = "Keep going!"
+    document.querySelector(".keydownLabel").textContent = "Keep going!"
+
+    //return array list with keys pressed
+    if (e.key > 0) {
+        keyArray.push(e.key)
+        console.log(keyArray);
+
+        document.querySelector(".newHTML").textContent = `${keyArray.join("")}`
+    }
 })  
 
 /* copy & paste */
@@ -39,19 +49,19 @@ copy.addEventListener("copy", e => {
 })
 
 paste.addEventListener("paste", e => {
-    document.querySelector("h3").innerText = "You successfully moved me!"
+    document.querySelector("h3").textContent = "You successfully moved me!"
 })
 
-/* drag & drop */
+/* dragenter */
 function handleDragStart(e) {
     e.dataTransfer.setData("text", this.id); 
 }
 
 function handleDragEnterLeave(e) {
     if(e.type == "dragenter") {
-      this.className = "drag-enter" 
+      this.className = "drag-enter";
     } else {
-      this.className = "" 
+      this.className = "";
     }
 }
 
@@ -87,3 +97,29 @@ for(let i = 0; i < target.length; i++) {
     target[i].addEventListener("dragenter", handleDragEnterLeave);
     target[i].addEventListener("dragleave", handleDragEnterLeave);
 }
+
+/* submit */
+const logIn = document.querySelector("#logIn");
+const submitBtn = document.querySelector("#submit");
+const logInInputs = logIn.querySelectorAll("input");
+
+logInInputs.forEach(input => {
+    submitBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (input.id == "email" && input.value.includes("@")) {
+            input.classList.add("colorValid");
+            logIn.querySelector(".firstParagraph").textContent = "✅ Valid email address";
+        } else if (input.id == "email" && !input.value.includes("@")) {
+            input.classList.add("colorInvalid");
+            logIn.querySelector(".firstParagraph").textContent = "❗ Please provide a valid email address";
+
+        } else if (input.id == "password" && input.value.length < 5) {
+            input.classList.add("colorInvalid");
+            logIn.querySelector(".secondParagraph").textContent = "❗ Strength of password: Very weak. Try again";
+        } else if (input.id == "password" && input.value.length > 15) {
+            input.classList.add("colorValid");
+            logIn.querySelector(".secondParagraph").textContent = "✅ Strength of password: Very strong";
+        }
+    })
+})
